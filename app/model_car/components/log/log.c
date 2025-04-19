cc920c4c15bd94ad83f004e21b3a055f57065b3f
andy666chang <andy850821@gmail.com>
2025-04-19 23:18:03 +0800

    refactor(log): refine log module

--- app/model_car/components/log/log.c
+++ app/model_car/components/log/log.c
@@ -4,20 +4,7 @@
 
 #include "components/log/log.h"
 
-static const char *log_level_name[] = {
-    "none", "error", "warn", "info", "debug", "verbose",
-};
-static log_system_ms log_system_cb;
 static log_level_t g_log_level = LOG_INFO;
-
-void log_route(log_system_ms system_ms) { log_system_cb = system_ms; }
-
-uint32_t log_timestamp() {
-    if (log_system_cb != NULL)
-        return log_system_cb();
-    else
-        return 0;
-}
 
 void log_write(log_level_t level, const char *tag, const char *format,
                   ...) {
@@ -35,12 +22,4 @@ void log_write(log_level_t level, const char *tag, const char *format,
         extern void tx_data_push(uint8_t *data, uint32_t len);
         tx_data_push(buf, len);
     }
-}
-
-void log_set_level(log_level_t level) { g_log_level = level; }
-
-log_level_t log_get_level(void) { return g_log_level; }
-
-const char *log_get_log_level_name(log_level_t level) {
-    return log_level_name[level];
 }
