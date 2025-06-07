dcfef121a7df08273ab5b337659c1296429b2967
andy666chang <andy850821@gmail.com>
2025-06-07 17:52:11 +0800

    feat: add led and log

--- app/street_lamp/street_lamp/street_lamp.ino
+++ app/street_lamp/street_lamp/street_lamp.ino
@@ -1,37 +1,91 @@
-/* This example shows how to use continuous mode to take
-range measurements with the VL53L0X. It is based on
-vl53l0x_ContinuousRanging_Example.c from the VL53L0X API.
-
-The range readings are in units of mm. */
 
 #include <Wire.h>
 #include <VL53L0X.h>
 
-VL53L0X sensor;
+#define SEN_NUM 1
+
+VL53L0X sensors[SEN_NUM];
+
+#define LED_PIN A0
+uint8_t led_pin[SEN_NUM] = {A0};
+uint32_t time_cnt[SEN_NUM];
 
 void setup()
 {
-  Serial.begin(9600);
-  Wire.begin();
+  Serial.begin(115200);
 
-  sensor.setTimeout(500);
-  if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1) {}
+  for (uint8_t i = 0; i < SEN_NUM; i++) {
+    pinMode( led_pin[i], OUTPUT );
+    digitalWrite( led_pin[i], LOW );
+
+    time_cnt[i] = 0;
   }
 
-  // Start continuous back-to-back mode (take readings as
-  // fast as possible).  To use continuous timed mode
-  // instead, provide a desired inter-measurement period in
-  // ms (e.g. sensor.startContinuous(100)).
-  sensor.startContinuous();
+  Wire.begin();
+  Wire.setClock(200000);
+
+  for (uint8_t i = 0; i < SEN_NUM; i++) {
+    sensors[i].setTimeout(50);
+    if (!sensors[i].init())
+    {
+      Serial.println("Failed to detect and initialize sensor!");
+      while (1) {}
+    }
+
+    // Start continuous back-to-back mode (take readings as
+    // fast as possible).  To use continuous timed mode
+    // instead, provide a desired inter-measurement period in
+    // ms (e.g. sensor.startContinuous(100)).
+    sensors[i].startContinuous();
+  }
 }
 
 void loop()
 {
-  Serial.print(sensor.readRangeContinuousMillimeters());
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  int min = 100, max = 250;
+  // Serial.print("min: ");
+  Serial.print(min);
+  Serial.print(" ");
+
+  // Serial.print("max: ");
+  Serial.print(max);
+  Serial.print(" ");
+
+  for (uint8_t i = 0; i < SEN_NUM; i++) {
+    // Serial.print("Dis: ");
+    uint16_t data = sensors[i].readRangeContinuousMillimeters();
+    // Serial.print("mm");
+    if (sensors[i].timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+    if (data > 250) {
+      data = 250;
+    } else if (data < 100) {
+      data = 100;
+    }
+
+    // Serial.print("data: ");
+    Serial.print(data);
+    Serial.print(" ");
+
+    // Serial.print("flag: ");
+    if (data < 180) {
+      Serial.print("200 ");
+      time_cnt[i] = millis();
+      digitalWrite( led_pin[i], HIGH );
+    } else {
+      Serial.print("100 ");
+    }
+  }
 
   Serial.println();
+
+  uint16_t ms = millis();
+  for (uint8_t i = 0; i < SEN_NUM; i++) {
+    if (ms - time_cnt[i] > 100) {
+      digitalWrite( led_pin[i], LOW );
+    }
+  }
+
+
+  _delay_ms(10);
 }
