1d63cb227d17da6c7e6c934742d507a7a2632c3a
andy666chang <andy850821@gmail.com>
2025-12-26 15:11:23 +0800

    feat(app/model_car): implement crc32 for config data

--- app/model_car/services/throttle/config.c
+++ app/model_car/services/throttle/config.c
@@ -10,24 +10,25 @@
 #include "interfaces/interface.h"
 
 #include "components/log/log.h"
-
+#include "crc.h"
 #define TAG "CFG"
 
 #define CFG_ADDR 0x0800F000
 #define MAGIC 0xA1234567
 
 typedef struct cfg_pack_t {
+    uint32_t header;
+
     union pack_t {
         prj_cfg_t cfg;
-        uint8_t dummy[252];
-    };
+        uint8_t dummy[248];
+    } pack;
 
-    union pack_t pack;
-    uint32_t check; // CRC32
+    uint32_t crc32;
 } cfg_pack_t;
 
 static const prj_cfg_t default_cfg = {
-    .version = 0,
+    .version = 1,
     .dir = 1,
     .center = 1500,
     .margin = 10,
@@ -37,31 +38,31 @@ static const prj_cfg_t default_cfg = {
     .bar_idx = 0,
 };
 
-static cfg_pack_t cfg_pack = {.check = 0x55};
-prj_cfg_t *prj_cfg = NULL;
+static cfg_pack_t cfg_pack;
+prj_cfg_t *prj_cfg = &cfg_pack.pack.cfg;
 
 void config_init(void) {
-    // LOGI(TAG, "size of prj_cfg: %d", sizeof(prj_cfg_t)); // 6
-    // LOGI(TAG, "size of cfg_pack: %d", sizeof(cfg_pack_t)); // 256
+    LOGD(TAG, "size of prj_cfg: %d", sizeof(prj_cfg_t)); // 10
+    LOGD(TAG, "size of cfg_pack: %d", sizeof(cfg_pack_t)); // 256
 
     // Read config pack from flash
     load_config();
-    LOGI(TAG, "cfg_pack.check: 0x%08X", cfg_pack.check);
+    LOGI(TAG, "cfg_pack.header: 0x%08X", cfg_pack.header);
+    LOGI(TAG, "cfg_pack.crc32: 0x%08X", cfg_pack.crc32);
 
-    // TODO: verify data valid with CRC32
-    if (cfg_pack.check != MAGIC) {
+    // Verify data valid with CRC32
+    uint32_t crc = HAL_CRC_Calculate(&hcrc, (uint32_t *)&cfg_pack.pack,
+                                         sizeof(cfg_pack.pack));
+    if (cfg_pack.crc32 != crc || cfg_pack.header != MAGIC) {
         // Replace by default config
         LOGW(TAG, "Invalid data in flash, reset to default");
         memset(&cfg_pack, 0, sizeof(cfg_pack_t));
         memcpy(&cfg_pack.pack.cfg, &default_cfg, sizeof(prj_cfg_t));
-        cfg_pack.check = MAGIC;
+        cfg_pack.crc32 = MAGIC;
 
         // Erase and write to flash
         save_config();
     }
-    
-    // Load to global config
-    prj_cfg = &cfg_pack.pack.cfg;
 
     // Dump config
     dump_config();
@@ -72,8 +73,11 @@ void load_config(void) {
 }
 
 void save_config(void) {
-    // TODO: Caculate CRC32
-    cfg_pack.check = MAGIC;
+    cfg_pack.header = MAGIC;
+
+    // Calculate CRC32
+    cfg_pack.crc32 = HAL_CRC_Calculate(&hcrc, (uint32_t *)&cfg_pack.pack,
+                                         sizeof(cfg_pack.pack));
 
     // Erase and write to flash
     flash_erase(CFG_ADDR, sizeof(cfg_pack_t));
